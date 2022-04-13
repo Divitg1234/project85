77f62d05ee2a237f9bb150007c88dbbfce2bb208
Divitg1234 <72795893+Divitg1234@users.noreply.github.com>
2022-04-13 15:07:20 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAOBJoF069M1TMkwDL5XqlthucGqURuzYs",
+  authDomain: "spectagram-b8eda.firebaseapp.com",
+  databaseURL: "https://spectagram-b8eda-default-rtdb.firebaseio.com",
+  projectId: "spectagram-b8eda",
+  storageBucket: "spectagram-b8eda.appspot.com",
+  messagingSenderId: "320322489488",
+  appId: "1:320322489488:web:fb1469393f0c3636f45c15"
+};
+  
+  // Initialize Firebase
+ 
